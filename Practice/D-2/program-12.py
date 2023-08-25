3b4ed85a0e08603c5acd27721081386f0be7115a
Subinay <subinayp108@gmail.com>
2023-08-25 14:22:39 +0530

    new programs added upto 12

--- Practice/D-2/program-12.py
+++ Practice/D-2/program-12.py
@@ -6,16 +6,22 @@ for i in range(size):
     for j in range(size - i - 1):
         print('  ', end='')
     for j in range(i * 2 + 1):
-        print('* ', end='')
+        if(j == 0 or j == i * 2):
+            print('* ', end='')
+        else:
+            print('  ', end='')
     for j in range(size - i - 1):
         print('  ', end='')
     print()
-
+    
 for i in range(1, size):
     for j in range(i):
         print('  ', end='')
     for j in range((size - i) * 2 - 1):
-        print('* ', end='')
+        if(j == 0 or j == (size - i - 1) * 2):
+            print('* ', end='')
+        else:
+            print('  ', end='')
     for j in range(i):
         print('  ', end='')
     print()

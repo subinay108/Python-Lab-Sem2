c9fdb4519eacdca56ea72924b3e05f9907872df0
Subinay <subinayp108@gmail.com>
2023-11-10 12:36:26 +0530

    lab 9 program-3 updated

--- Practice/Lab-9/program-3.py
+++ Practice/Lab-9/program-3.py
@@ -1,10 +1,10 @@
 # Pass a tuple to the divmod() function and obtain the quotient and the remainder
-def divmod(dividend, divisor):
-    return (dividend // divisor, dividend % divisor)
+def divmod(tup):
+    return (tup[0] // tup[1], tup[0] % tup[1])
 
 dividend = float(input('Enter the dividend: '))
 divisor = float(input('Enter the divisor: '))
 
-quotient, remainder = divmod(dividend, divisor)
+quotient, remainder = divmod((dividend, divisor))
 
 print(f'Quotient: {quotient}, Remainder: {remainder}')

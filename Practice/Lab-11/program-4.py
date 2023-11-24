270953c5fc7de35a8058e29781e66b2bde75f96f
Subinay <subinayp108@gmail.com>
2023-11-24 14:27:01 +0530

    Lab 11 program-4 added

---
+++ Practice/Lab-11/program-4.py
@@ -0,0 +1,18 @@
+# WAP that defines a function inspalindrome() which checks whether a given string is a palindrome or
+# not. Ignore spaces and case mismatch while checking for palindrome
+
+string = input('Enter a string: ')
+
+def ispalindrome(string):
+    string = string.replace(' ', '') # removes spaces
+    string = string.upper() # convert the string to uppercase
+    
+    flag = True
+    for i in range(len(string)//2): # checks upto middle of the string
+        if string[i] != string[-(i + 1)]: # if the element from both ends are equal or not
+            flag = False
+            break
+    
+    return flag
+
+print('Given string is palindrome: ', ispalindrome(string))

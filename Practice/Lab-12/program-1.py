86a9d830317609e9eb72181e81d9817dfa4b893d
Subinay <subinayp108@gmail.com>
2023-12-01 13:12:18 +0530

    Lab 12 program-1 updated

--- Practice/Lab-12/program-1.py
+++ Practice/Lab-12/program-1.py
@@ -1,19 +1,15 @@
+# Date: 01/12/2023
 # If a positive integer is entered through the keyboard, write a recursive function to obtain the prime
 # factors of the number
 
-def isPrime(n):
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-def getPrimeFactors(n):
-    for i in range(2, n + 1):
-        if isPrime(i):
-            if n % i == 0:
-                print(i, end=' ')
-                getPrimeFactors(n//i)
-                break
+def getPrimeFactors(n, count = 2):
+    if n <= 1:
+        return 
+    elif n % count == 0:
+        print(count, end = ' ')
+        getPrimeFactors(n // count)
+    else:
+        getPrimeFactors(n, count + 1)
 
     
 
